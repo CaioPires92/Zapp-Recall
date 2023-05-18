76c0d1d569812dc81d00accda5ff9e3df185b83e
Caio Pires <caiogodoi_92@hotmail.com>
2023-05-18 13:23:58 -0300

    clicar card e renderizar pergunta

--- src/components/TextCamp.jsx
+++ src/components/TextCamp.jsx
@@ -1,90 +1,108 @@
+import { useState } from 'react'
 import setaPlay from '../assets/seta_play.png'
 import setaVirar from '../assets/seta_virar.png'
 import styled from 'styled-components'
 
-const camps = [
-  {
-    id: 1,
-    texto: 'Pergunta 1',
-    question: 'O que é JSX?',
-    answer: 'Uma extensão da linguagem JavaScript'
-  },
-  {
-    id: 2,
-    texto: 'Pergunta 2',
-    question: 'O React é __',
-    answer: 'Uma biblioteca JavaScript para construção de interfaces'
-  },
-  {
-    id: 3,
-    texto: 'Pergunta 3',
-    question: 'Componentes devem iniciar com __',
-    answer: 'Letra maiúscula'
-  },
-  {
-    id: 4,
-    texto: 'Pergunta 4',
-    question: 'Podemos colocar __ dentro do JSX',
-    answer: 'expressões'
-  },
-  {
-    id: 5,
-    texto: 'Pergunta 5',
-    question: 'O ReactDOM nos ajuda __',
-    answer: 'Interagindo com a DOM para colocar componentes React na mesma'
-  },
-  {
-    id: 6,
-    texto: 'Pergunta 6',
-    question: 'Usamos o npm para __',
-    answer: 'Gerenciar os pacotes necessários e suas dependências'
-  },
-  {
-    id: 7,
-    texto: 'Pergunta 7',
-    question: 'Usamos props para __',
-    answer: 'Passar diferentes informações para componentes'
-  },
-  {
-    id: 8,
-    texto: 'Pergunta 8',
-    question: 'Usamos estado (state) para __',
-    answer:
-      'Dizer para o React quais informações quando atualizadas devem renderizar a tela novamente'
-  }
-]
-
-export function CardVazio() {
+export function CardVazio({ texto, onClick }) {
   return (
-    <>
-      {camps.map(camp => (
-        <SCTextCampVazio key={camp.texto}>
-          <p>{camp.texto}</p>
-          <img src={setaPlay} alt="icone" />
-        </SCTextCampVazio>
-      ))}
-    </>
+    <SCTextCampVazio>
+      <p>{texto}</p>
+      <img src={setaPlay} alt="icone" onClick={onClick} />
+    </SCTextCampVazio>
   )
 }
 
-export function CardPergunta() {
+export function CardPergunta({ question }) {
   return (
-    <>
-      {camps.map(camp => (
-        <SCTextCampPergunta key={camp.texto}>
-          <p>{camp.question}</p>
-          <img src={setaVirar} alt="icone" />
-        </SCTextCampPergunta>
-      ))}
-    </>
+    <SCTextCampPergunta>
+      <p>{question}</p>
+      <img src={setaVirar} alt="icone" />
+    </SCTextCampPergunta>
   )
 }
 
 export default function TextCamp() {
+  const [camps, setCamps] = useState([
+    {
+      id: 1,
+      texto: 'Pergunta 1',
+      question: 'O que é JSX?',
+      answer: 'Uma extensão da linguagem JavaScript',
+      showPergunta: false
+    },
+    {
+      id: 2,
+      texto: 'Pergunta 2',
+      question: 'O React é __',
+      answer: 'Uma biblioteca JavaScript para construção de interfaces',
+      showPergunta: false
+    },
+    {
+      id: 3,
+      texto: 'Pergunta 3',
+      question: 'Componentes devem iniciar com __',
+      answer: 'Letra maiúscula',
+      showPergunta: false
+    },
+    {
+      id: 4,
+      texto: 'Pergunta 4',
+      question: 'Podemos colocar __ dentro do JSX',
+      answer: 'expressões',
+      showPergunta: false
+    },
+    {
+      id: 5,
+      texto: 'Pergunta 5',
+      question: 'O ReactDOM nos ajuda __',
+      answer: 'Interagindo com a DOM para colocar componentes React na mesma',
+      showPergunta: false
+    },
+    {
+      id: 6,
+      texto: 'Pergunta 6',
+      question: 'Usamos o npm para __',
+      answer: 'Gerenciar os pacotes necessários e suas dependências',
+      showPergunta: false
+    },
+    {
+      id: 7,
+      texto: 'Pergunta 7',
+      question: 'Usamos props para __',
+      answer: 'Passar diferentes informações para componentes',
+      showPergunta: false
+    },
+    {
+      id: 8,
+      texto: 'Pergunta 8',
+      question: 'Usamos estado (state) para __',
+      answer:
+        'Dizer para o React quais informações quando atualizadas devem renderizar a tela novamente',
+      showPergunta: false
+    }
+  ])
+
+  const handleCardClick = id => {
+    const updatedCamps = camps.map(camp =>
+      camp.id === id ? { ...camp, showPergunta: true } : camp
+    )
+    setCamps(updatedCamps)
+  }
+
   return (
     <>
-      {/* <CardVazio /> */}
-      <CardPergunta />
+      {camps.map(camp => (
+        <div key={camp.id}>
+          {camp.showPergunta ? (
+            <CardPergunta question={camp.question} />
+          ) : (
+            <CardVazio
+              texto={camp.texto}
+              onClick={() => handleCardClick(camp.id)}
+            />
+          )}
+        </div>
+      ))}
     </>
   )
 }
